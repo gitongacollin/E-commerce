f3123d0f693d031a1a498c947460d2cb6138285e
gitongacollin <gitongacollin@users.noreply.github.com>
2019-09-17 14:09:04 +0300

    Update
    Installation of Spatie ACL and Categories side panel working

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,38 +20,34 @@ class IndexController extends Controller
 
 
 
-    	//Get all Categories and sub categories
-    	$categories = Category::where(['parent_id'=>0])->get();
-    	//$categories = json_decode(json_encode($categories));
-    	//echo "<pre>";print_r($categories); die;
-    	$catgories_menu = "";
-    	foreach ($categories as $cat) {
-    		$catgories_menu .= "<div class= 'panel-heading'>
-									<h4 class='panel-title'>
-										<a data-toggle='collapse' data-parent='#".$cat->id."' href='#".$cat->url."'>
-											<span class='badge pull-right'><i class='fa fa-plus'></i></span>
-											".$cat->name."
-										</a>
-									</h4>
-								</div>
-								<div id='#".$cat->id."' class='panel-collapse collapse'>
-									<div class='panel-body'>
-										<ul>";
-							    		$sub_categories = Category::where(['parent_id'=>$cat->id])->get();
-							    		foreach($sub_categories as $subcat){
-											$catgories_menu .= "<li><a href='#'>".$subcat->name." </a></li>";
-										}
-										$catgories_menu .= "</ul>
-									</div>
-								</div>"; 
-    		
-    	}
+    	// Get All Categories and Sub Categories
+    	$categories = Category::with('categories')->where(['parent_id' => 0])->get();
+    	/*$categories = json_decode(json_encode($categories));
+    	echo "<pre>"; print_r($categories); die;*/
+    	/*$categories_menu = "";
+		foreach($categories as $cat){
+			$categories_menu .= "
+			<div class='panel-heading'>
+				<h4 class='panel-title'>
+					<a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+						<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+						".$cat->name."
+					</a>
+				</h4>
+			</div>
+			<div id='".$cat->id."' class='panel-collapse collapse'>
+				<div class='panel-body'>
+					<ul>";
+					$sub_categories = Category::where(['parent_id' => $cat->id])->get();
+					foreach($sub_categories as $sub_cat){
+						$categories_menu .= "<li><a href='#'>".$sub_cat->name." </a></li>";
+					}
+						$categories_menu .= "</ul>
+				</div>
+			</div>
+			";		
+		}*/
 
-
-
-
-
-
-    	return view('index')->with(compact('productsAll','catgories_menu'));
+    	return view('index')->with(compact('productsAll','categories'));
     }
 }
